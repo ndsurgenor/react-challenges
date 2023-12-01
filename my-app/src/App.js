06fe7b8c870cf6c1e96c332647c304c65d637f24
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-01 11:54:53 +0000

    Add Content.js and create map()

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>      
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
